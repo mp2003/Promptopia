d50e7c4da5c992d7bac5f9fafd9afa70406989bb
mahi5256 <m.p6092003@gmail.com>
2023-10-12 11:55:19 +0530

    working on next js

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 const Home = () => {
-  return (
-    <div>hello world </div>
-  )
-}
+  return <section className=" w-full flex-center flex-col">
+    <h1 className="head_text text-center"> Discover & share
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+    </h1>
+    <p className="desc text-center">
 
-export default Home
+    </p>
+  </section>;
+};
+
+export default Home;
