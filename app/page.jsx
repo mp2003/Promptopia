bcdd2722568982ddf425a18c95ba1f7bfe3a8130
mahi5256 <m.p6092003@gmail.com>
2023-10-12 11:56:00 +0530

    working on next js

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,8 @@ const Home = () => {
       <span className="orange_gradient text-center"> AI-Powered Prompts</span>
     </h1>
     <p className="desc text-center">
-
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
     </p>
   </section>;
 };
