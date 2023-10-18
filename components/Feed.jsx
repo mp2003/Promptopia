87d00211951e46caaeac4fa1c0a14f950469c871
mahi5256 <m.p6092003@gmail.com>
2023-10-18 10:07:49 +0530

    creating feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,10 +2,61 @@
 import React, { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
+const PromptCardList = ({ data, handelTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => {
+        <PromptCard
+          key={post._id}
+          post={post}
+          handelTagClick={handelTagClick}
+        />
+
+      })}
+    </div>
+  )
+}
+
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([])
+  const handelSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+
+    fetchPost();
+  }, [])
+
+
+
   return (
     <section className='feed'>
-      <form className='relative w-full flex-center'></form>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='search for a tag or a username'
+          value={searchText}
+          onChange={handelSearchChange}
+          required
+          className='search_input peer'
+        />
+
+      </form>
+      <PromptCardList
+        data={[posts]}
+        handelTagClick={() => { }}
+
+      />
+
     </section>
 
   )
