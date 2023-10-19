23ff467e94893be5f75f2244bb8abf65155356f6
mahi5256 <m.p6092003@gmail.com>
2023-10-19 08:29:44 +0530

    addind edit feature

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import PromptCard from './PromptCard'
-const Profile = ({ name, desc, data, handelEdit, handelDelete }) => {
+import PromptCard from "./PromptCard";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
         <span className='blue_gradient'>{name} Profile</span>
       </h1>
       <p className='desc text-left'>{desc}</p>
+
       <div className='mt-10 prompt_layout'>
         {data.map((post) => (
           <PromptCard
             key={post._id}
             post={post}
-            handelEdit={() => handelEdit && handelEdit(post)}
-            handelDelete={() => handelDelete && handelDelete(post)}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
           />
-
         ))}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,82 +1,96 @@
 "use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { useSession } from 'next-auth/react';
-import { usePathname, useRouter } from 'next/navigation';
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-
-const PromptCard = ({ post, handelTagClick, handelEdit, handelDelete }) => {
-  const [copied, setCopied] = useState("");
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
+  const [copied, setCopied] = useState("");
 
+  const handleProfileClick = () => {
+    console.log(post);
 
-  const handelCopy = (prompt) => {
-    setCopied(prompt);
-    navigator.clipboard.writeText(prompt);
-    setTimeout(() => { setCopied(""); }, 4000);
-  }
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(false), 3000);
+  };
 
   return (
     <div className='prompt_card'>
-      <div className="flex justify-between items-start gap-5">
-        {post.creator ? (
-          <div>
-            <div className='flex-1 flex-justify-start items-center gap-3 cursor-pointer'>
-              <Image
-                src={post.creator.image}
-                alt='user_image'
-                width={40}
-                height={40}
-                className='rounded-full object-contain'
-              />
-              <div className='flex flex-col'>
-                <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
-                <p className='font-inner text-sm text-gray-500'>{post.creator.email}</p>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <p className='font-inner text-sm text-gray-500'>No creator information available.</p>
-        )}
-        <div className='copy_btn' onClick={() => { handelCopy(post.prompt) }}>
+      <div className='flex justify-between items-start gap-5'>
+        <div
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+          onClick={handleProfileClick}
+        >
           <Image
-            src={copied === post.prompt
-              ? '/assets/icons/tick.svg' :
-              '/assets/icons/copy.svg'
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-gray-900'>
+              {post.creator.username}
+            </h3>
+            <p className='font-inter text-sm text-gray-500'>
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className='copy_btn' onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
             }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
-      <div>
-        <p className=' my-4 font-staoshi text-sm text-gray-400'>{post.prompt}</p>
-        <p className=' font-inter text-sm blue_gradient '
-          onClick={() => { handelTagClick && handelTagClick(post.tag) }}
-        >{post.tag}</p>
-        {session?.user.id === post.creator._id && pathName === '/profile' && (
-          <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
-            <p className=' font-inter text-sm green_gradient cusrsor-pointer'
-              onClick={handelEdit}
-            >
-              Edit
-            </p>
-            <p className=' font-inter text-sm orange_gradient cusrsor-pointer'
-              onClick={handelDelete}
-            >
-              Delete
-            </p>
 
-          </div>
-        )}
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
 
-      </div>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default PromptCard;
